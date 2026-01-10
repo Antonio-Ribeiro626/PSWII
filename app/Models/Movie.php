b6afe3cb151657e85516265e40d78de64e42b311
Antonio Ribeiro <antonioribeiro626185@gmail.com>
2026-01-10 03:38:23 +0000

    imagem de fundo esta a ser salva na base de dados com sucesso, cartaz a ser criado

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     protected $fillable = [
-    'tmdb_id',
-    'title',
-    'overview',
-    'poster_path',
-    'release_date',
-    'rating',
+     'tmdb_id',
+        'title',
+        'overview',
+        'poster_path',
+        'backdrop_path',
+        'genres',
+        'release_date',
+        'rating',
     ];
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,16 +3,32 @@
 
 @section('content')
 
+@php
+    $movieDesc = $movies->first();
+    
+@endphp
+
+<div class="Cartaz">
+    <h2>{{ $movieDesc->title }}</h2>
+    <p>{{ $movieDesc->overview }}</p>
+    <img src="https://image.tmdb.org/t/p/w500{{ $movieDesc->backdrop_path }}" alt="" srcset="">
+</div>
+
+
+
 <h2>Populares</h2>
 <div class="cards">
     @foreach ( $movies as $movie )
         <div class="card">
             @if($movie->poster_path)
-                <img src="https://image.tmdb.org/t/p/w500{{ $movie->poster_path }}" width="270" alt="poster path">
+                <img src="https://image.tmdb.org/t/p/w500{{ $movie->poster_path }}"  alt="poster path">
             @endif
-                <p>{{ $movie->title }}</p>
-                <p>{{ $movie->release_date}}</p>
-                <p>{{ $movie->genres ?? 'Sem Generos' }}</p>
+                <div class="text-card">
+                    <h3>{{ $movie->title }}</h3>
+                    <p>{{ $movie->genres ?? 'Sem Generos' }}</p>
+                    <p>{{ $movie->release_date}}</p>
+                </div>
+         
         </div>
     @endforeach
 </div>
